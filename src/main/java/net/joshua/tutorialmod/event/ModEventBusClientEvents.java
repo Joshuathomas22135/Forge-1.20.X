0aeb1324ef811c44cdd87d7f4a38907f910d3e4f
Joshuathomas22135 <joshuatjeremiah22135@outlook.com>
2024-04-08 13:37:07 +0400

    updated

---
+++ src/main/java/net/joshua/tutorialmod/event/ModEventBusClientEvents.java
@@ -0,0 +1,24 @@
+package net.joshua.tutorialmod.event;
+
+import net.joshua.tutorialmod.TutorialMod;
+import net.joshua.tutorialmod.block.entity.ModBlockEntities;
+import net.joshua.tutorialmod.block.entity.renderer.GemPolishingBlockEntityRenderer;
+import net.joshua.tutorialmod.entity.client.ModModelLayers;
+import net.joshua.tutorialmod.entity.client.RhinoModel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModEventBusClientEvents {
+    @SubscribeEvent
+    public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
+    }
+
+    @SubscribeEvent
+    public static void registerBER(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(ModBlockEntities.GEM_POLISHING_BE.get(), GemPolishingBlockEntityRenderer::new);
+    }
+}
---
+++ src/main/java/net/joshua/tutorialmod/event/ModEventBusEvents.java
@@ -0,0 +1,16 @@
+package net.joshua.tutorialmod.event;
+
+import net.joshua.tutorialmod.TutorialMod;
+import net.joshua.tutorialmod.entity.ModEntities;
+import net.joshua.tutorialmod.entity.custom.RhinoEntity;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.RHINO.get(), RhinoEntity.createAttributes().build());
+    }
+}
