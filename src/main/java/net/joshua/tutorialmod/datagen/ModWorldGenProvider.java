b17a3814b458e51318f92022f8bfaf21b7d7e7c3
Joshuathomas22135 <joshuatjeremiah22135@outlook.com>
2024-06-28 13:55:46 +0400

    Custom Dimension

--- src/main/java/net/joshua/tutorialmod/datagen/ModWorldGenProvider.java
+++ src/main/java/net/joshua/tutorialmod/datagen/ModWorldGenProvider.java
@@ -5,6 +5,7 @@ import net.joshua.tutorialmod.worldgen.ModBiomeModifiers;
 import net.joshua.tutorialmod.worldgen.ModConfiguredFeatures;
 import net.joshua.tutorialmod.worldgen.ModPlacedFeatures;
 import net.joshua.tutorialmod.worldgen.biome.ModBiomes;
+import net.joshua.tutorialmod.worldgen.dimension.ModDimensions;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -17,10 +18,12 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.DIMENSION_TYPE, ModDimensions::bootstrapType)
             .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
             .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
             .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap)
-            .add(Registries.BIOME, ModBiomes::boostrap);
+            .add(Registries.BIOME, ModBiomes::boostrap)
+            .add(Registries.LEVEL_STEM, ModDimensions::bootstrapStem);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(TutorialMod.MOD_ID));
